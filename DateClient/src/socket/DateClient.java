5a379cd9db552fe81aaf50733983f007fe2522eb
usuario <usuario@isunah.net>
2018-11-22 08:00:19 -0600

    Cambio del server y client

--- DateClient/src/socket/DateClient.java
+++ DateClient/src/socket/DateClient.java
@@ -13,16 +13,22 @@ import javax.swing.JOptionPane;
 
 public class DateClient {
 
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException, InterruptedException{
         String serverAddress = JOptionPane.showInputDialog(
                 "Ingrese la IP de la maquina en donde \n"
    + "está siendo ejecutado el servidor de frcha y hora en el puerto 9090:  ");
         Socket s =new Socket (serverAddress, 9090);
+          PrintWriter out = new PrintWriter(s.getOutputStream(),true);
         BufferedReader input = new BufferedReader(new InputStreamReader(s.getInputStream()));
         String answer =input.readLine();
         JOptionPane.showMessageDialog(null, answer);
+        String nombre_archivo = JOptionPane.showInputDialog("Ingrese el nombre del archivo remoto: "); 
+        out.println(nombre_archivo); 
+        Thread.sleep(100);
+        
+      while(input.ready())
+          System.out.println(input.readLine());
+        System.err.println("Jorge Coello");
         System.exit(0);
-        PrintWriter pw = new PrintWriter(s.getOutputStream(),true);
-        pw.println("Jorge Coello"); 
     }   
 }
--- DateServer/src/dateserver/DateServer.java
+++ DateServer/src/dateserver/DateServer.java
@@ -1,11 +1,15 @@
 
 package dateserver;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Date;
+import java.util.Scanner;
 
 
 public class DateServer {
@@ -18,11 +22,21 @@ public class DateServer {
                 System.out.println("Servidor esperando conexion de cliente...");
                 Socket socket = listener.accept();
                 System.out.println("Conexion Aceptada");
+                Scanner entrada = null;
                 try{
                     System.out.println("Enviando paquete con Fecha y Hora");
                     PrintWriter out = new PrintWriter(socket.getOutputStream(),true);
                     out.println(new Date().toString());
+                    
+                    BufferedReader input = new BufferedReader (new InputStreamReader(socket.getInputStream()));
+                    entrada = new Scanner(new FileReader(input.readLine()));
+                    
+                    while(entrada.hasNext()){
+                        out.println(entrada.nextLine());
+                        System.err.println(input.readLine());
+                    }
                 }finally {
+                    entrada.close();
                     socket.close();
                     System.out.println("Conexion con el servidor terminada...");
                 }
